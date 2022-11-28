cb6d9c29f339a6c6c6d9b3f032de1e93115c26bb
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-11-28 11:58:22 +0700

    style: update margin & padding component menu 'kebijakan privasi dan ketentuan pengguna'

--- components/KebijakanPrivasiKetentuanPengguna/Menu.vue
+++ components/KebijakanPrivasiKetentuanPengguna/Menu.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="border-b-2 border-gray-100 dark:border-[#292C2A] mb-[24px] lg:dark:border-0 lg:mx-5 xl:mx-3">
-    <div class="lg:flex justify-center items-start p-[16px]">
-      <div class="flex-shrink-0">
-        <component :is="icon" class="icon" />
-      </div>
-      <div class="lg:ml-5 mt-5 lg:mt-0 min-w-0 flex-1">
-        <h1 class="uppercase text-[16px] font-roboto font-bold">
-          {{ title }}
-        </h1>
-        <p class="text-[14x] font-lato mt-3 mb-3 text-[#424242]">
-          {{ description }}
-        </p>
-        <NuxtLink :to="link" class="font-lato text-green-600 font-bold text-[14px] mb-3">
-          Baca {{ title }} Kami
-        </NuxtLink>
-      </div>
+  <div class="lg:flex justify-center items-start p-[16px]">
+    <div class="flex-shrink-0">
+      <component :is="icon" class="icon" />
+    </div>
+    <div class="lg:ml-5 mt-5 lg:mt-0 min-w-0 flex-1">
+      <h1 class="uppercase text-[16px] font-roboto font-bold">
+        {{ title }}
+      </h1>
+      <p class="text-[14x] font-lato mt-3 mb-3 text-[#424242]">
+        {{ description }}
+      </p>
+      <NuxtLink :to="link" class="font-lato text-green-600 font-bold text-[14px] mb-3">
+        Baca {{ title }} Kami
+      </NuxtLink>
     </div>
   </div>
 </template>
