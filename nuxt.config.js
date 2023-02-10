75b23933ef2f730f6e6ffe25d22ec968c438247b
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-02-10 16:23:29 +0700

    chore: remove sentry package and configuration in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,28 +60,8 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://sentry.nuxtjs.org/
-    '@nuxtjs/sentry'
+    '@nuxtjs/axios'
   ],
-
-  // sentry config
-  sentry: {
-    dsn: process.env.SENTRY_DSN, // Enter your project's DSN here
-    tracing: {
-      tracesSampleRate: parseFloat(process.env.SENTRY_SAMPLE_RATE),
-      vueOptions: {
-        tracing: true,
-        tracingOptions: {
-          hooks: ['mount', 'update'],
-          timeout: 2000,
-          trackComponents: true
-        }
-      },
-      browserOptions: {}
-    },
-    disabled: process.env.SENTRY_ENABLED === 'false'
-  },
 
   // google fonts
   googleFonts: {
