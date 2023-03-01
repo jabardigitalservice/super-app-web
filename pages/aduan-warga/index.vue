fb639392806356b1f2ec510c64a279d970a6a0a1
Ray Fajar S <rayfajars.com>
2023-03-01 12:03:44 +0700

    feat: add pages aduan warga home & detail

---
+++ pages/aduan-warga/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen bg-background dark:bg-black">
+    <div class="h-full block sm:flex sm:flex-col sm:justify-center sm:items-center">
+      <AduanWarga />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AduanWarga',
+  components: { }
+
+}
+</script>
