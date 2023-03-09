488a836b435550b022f9bc9f9c7fef5a5f5c17d9
Ray Fajar S <rayfajars@gmail.com>
2023-03-09 21:53:23 +0700

    feat: add component alert

--- components/Base/Dialog/index.vue
+++ components/Base/Dialog/index.vue
@@ -8,57 +8,53 @@
     leave-from-class="opacity-100"
     leave-to-class="opacity-0"
   >
-    <div
-      v-show="showPopup"
-      class="fixed w-full h-1/2 md:h-1/3  bottom-0 bg-white rounded-tr-2xl rounded-tl-2xl overflow-y-auto"
-    >
-      <!-- HEADER -->
-      <div class="h-[50px] bg-white py-[16px] px-[24px] rounded-t-lg">
-        <h1 class="font-roboto text-[16px] font-bold text-green-600">
-          {{ title }}
-        </h1>
+    <div v-show="showPopup" class="fixed w-full bottom-0">
+      <div class="flex mr-5">
+        <BaseButton
+          class="!rounded-full !bg-green-600 !w-[44px] !h-[44px] border-0 mb-4 ml-auto"
+          @click="closeHandle"
+        >
+          <BaseIconSvg :icon="`/icon/close-button.svg`" class="!bg-white" />
+        </BaseButton>
       </div>
 
-      <!-- body -->
-      <div class="px-[24px]">
-        <p class="font-lato text-size[14px] text-gray-800 mt-6 md:mt-8 mb-4">
-          <slot v-if="$slots['confirmationText']" name="confirmationText" />
-        </p>
-        <div
-          v-if="informationMessage"
-          class="py-[16px] px-[12px] font-lato text-gray-800 bg-[#F9FBFC] border rounded-lg border-[#EFF1F3]"
-        >
-          <div class="flex justify-start items-start">
-            <BaseIconSvg
-              :icon="`/icon/info.svg`"
-              class="!w-[24px] !h-[24px] !bg-green-600"
-            />
-
-            <div class="ml-[13px]">
-              <p class="text-[14px] text-gray-800 font-lato">
-                {{ informationMessage }}
-              </p>
-            </div>
-          </div>
+      <div
+        class="bg-white rounded-tr-2xl rounded-tl-2xl overflow-y-auto max-h-[calc(100vh-64px-120px)] "
+      >
+        <!-- HEADER -->
+        <div class="h-[50px] bg-white py-[16px] px-[24px] rounded-t-lg">
+          <h1 class="font-roboto text-[16px] font-bold text-green-600">
+            {{ title }}
+          </h1>
         </div>
-      </div>
 
-      <!-- FOOTER -->
-      <div class="h-fit px-[24px] justify-center py-[16px]">
-        <BaseButton
-          v-if="labelButtonApprove"
-          class="border w-full bg-green-600 text-white rounded-lg text-[14px] font-lato"
-          @click="submitHandle"
-        >
-          {{ labelButtonApprove }}
-        </BaseButton>
-        <BaseButton
-          v-if="labelButtonReject"
-          class="w-full border-none bg-white text-green-700 text-[14px] font-lato mt-4"
-          @click="submitHandle"
-        >
-          {{ labelButtonReject }}
-        </BaseButton>
+        <!-- body -->
+        <div class="px-[24px]">
+          <p class="font-lato text-size[14px] text-gray-800 mt-6 md:mt-8 mb-4">
+            <slot />
+          </p>
+
+          <!-- allert message -->
+          <BaseAlert :message="informationMessage" />
+        </div>
+
+        <!-- FOOTER -->
+        <div class="h-fit px-[24px] justify-center py-[16px]">
+          <BaseButton
+            v-if="labelButtonApprove"
+            class="border w-full bg-green-600 text-white rounded-lg text-[14px] font-lato"
+            @click="submitHandle"
+          >
+            {{ labelButtonApprove }}
+          </BaseButton>
+          <BaseButton
+            v-if="labelButtonReject"
+            class="w-full border-none bg-white text-green-700 text-[14px] font-lato mt-4"
+            @click="submitHandle"
+          >
+            {{ labelButtonReject }}
+          </BaseButton>
+        </div>
       </div>
     </div>
   </transition>
