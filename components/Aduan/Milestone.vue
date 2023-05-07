c4b08486bed19178cde84f988d5b466cb402b688
Ray Fajar S <rayfajars.com>
2023-05-07 23:01:50 +0700

    refactor: remove unused code

--- components/Aduan/Milestone.vue
+++ components/Aduan/Milestone.vue
@@ -501,7 +501,6 @@ export default {
 
     setLogSpanLapor (logSpan, idAduanSpanLapor) {
       this.$router.push(`/aduan-warga/log-span-lapor/${idAduanSpanLapor}`)
-      // this.$store.commit('setLogSpan', logSpan.reverse())
     }
   }
 }
