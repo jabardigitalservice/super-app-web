2bb2d1ba96841df344aca9b16d20d3026e0454aa
Ray Fajar S <rayfajars.com>
2023-05-08 10:46:09 +0700

    refactor: remove double space

--- components/Base/Spinner/index.vue
+++ components/Base/Spinner/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="showSpinner" class="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
-    <div class="border-t-transparent border-solid animate-spin  rounded-full border-green-600 border-8 h-14 w-14" />
+  <div v-if="showSpinner" class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
+    <div class="border-t-transparent border-solid animate-spin rounded-full border-green-600 border-8 h-14 w-14" />
   </div>
 </template>
 
