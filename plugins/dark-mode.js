7aaebd981af120ae9d49cb63ee7171be1271367f
Ray Fajar S <rayfajars.com>
2023-05-17 13:21:59 +0700

    feat:add default theme by device

--- plugins/dark-mode.js
+++ plugins/dark-mode.js
@@ -1,9 +1,13 @@
 export default ({ route }) => {
   if (process.client) {
-    if (route.query.dark) {
+    if (route.query.dark === 'true') {
       document.documentElement.classList.add('dark')
-    } else {
+    } else if (route.query.dark === 'false') {
       document.documentElement.classList.remove('dark')
+    } else {
+      // set theme by default device
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+      document.documentElement.classList.toggle('dark', prefersDark)
     }
   }
 }
