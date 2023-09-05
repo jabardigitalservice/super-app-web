6d7d6f186f188d913389337f74cede6b576e53a1
Ray Fajar Salinggih <rayfajars@gmail.com>
2023-09-05 16:41:16 +0700

    feat: add store file aduan warga and add page file aduan warga

---
+++ pages/aduan-warga/file-aduan/_type/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-full flex justify-center sm:h-auto">
+    <BaseImageZoom :src="$store.state.fileAduan" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileAduan',
+  head () {
+    return {
+      title: this.$route.params.type.replace('-', ' ').toUpperCase()
+    }
+  },
+  methods: {
+    zoomIn () {
+      console.log('zoom in')
+    },
+    zoomOut () {
+      console.log('zoom out')
+    }
+  }
+}
+</script>
