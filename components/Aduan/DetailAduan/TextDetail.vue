fa6235acb6320da5348940d2e6981ebada0b06fb
Ray Fajar Salinggih <rayfajars@gmail.com>
2023-09-12 14:42:20 +0700

    refactorL change tags span to h1

--- components/Aduan/DetailAduan/TextDetail.vue
+++ components/Aduan/DetailAduan/TextDetail.vue
@@ -1,3 +1,5 @@
 <template>
-  <span class="font-lato text-[14px] font-normal leading-5 text-gray-900 dark:text-dark-text-high"><slot /></span>
+  <h1 class="font-lato text-[14px] font-normal leading-5 text-gray-900 dark:text-dark-text-high">
+    <slot />
+  </h1>
 </template>
