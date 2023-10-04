7e248b94c04bac8783db7389fd8aadbcbc1d18b9
rayfajars <rayfajars@gmail.com>
2023-10-04 17:03:36 +0700

    feat: create reusable pages for dokumen bukti and file aduan

--- pages/aduan-warga/file-dokumen-bukti/index.vue
+++ pages/aduan-warga/file-dokumen-bukti/index.vue
@@ -1,40 +1,83 @@
 <template>
   <div class="h-screen bg-white dark:bg-dark-emphasis-low">
     <div class="w-full h-full">
-      <h2>File Dokumen Bukti</h2>
       <div v-for="(group, groupName) in grupByTypeFile" :key="groupName">
-        <h3>{{ groupName }}</h3>
-        <ul>
-          <li v-for="(url,index) in group" :key="index">
-            {{ url }}
-          </li>
-        </ul>
+        <template v-if="groupName === 'images'">
+          <ButtonDokumenBukti @go-to-page="gotPageFile(group.data)">
+            <div class="flex items-center">
+              <div>
+                <BaseIconSvg
+                  :icon="group.icon"
+                  class="!shadow-lg !w-[24.458px] !h-[32px] mr-3"
+                  mode="image"
+                />
+              </div>
+              <span
+                class="font-lato text-[14px] font-normal leading-5 text-gray-800 dark:text-dark-text-high mr-3"
+              >Gambar
+              </span>
+
+              <div
+                class="dark:bg-dark-emphasis-medium bg-gray-100 flex px-5.5 flex-col items-center gap-10 rounded-xl"
+              >
+                <span
+                  class="text-[12px] font-roboto font-medium leading-[18px] dark:text-dark-text-high text-gray-800 p-1"
+                >{{ group.data.length }} Terlampir</span>
+              </div>
+            </div>
+          </ButtonDokumenBukti>
+        </template>
+        <template v-else>
+          <ButtonDokumenBukti
+            v-for="(url, index) in group.data"
+            :key="index"
+            @go-to-page="gotPageFile(url)"
+          >
+            <div class="flex items-center">
+              <div>
+                <BaseIconSvg
+                  :icon="group.icon"
+                  class="!shadow-lg !w-[24.458px] !h-[32px] mr-3"
+                  mode="image"
+                />
+              </div>
+              <span
+                class="font-lato text-[14px] font-normal leading-5 text-gray-800"
+              >{{ getLastSegment(url) }}</span>
+            </div>
+          </ButtonDokumenBukti>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ButtonDokumenBukti from '~/components/Aduan/ButtonDokumenBukti'
 export default {
   name: 'FileDokumenBukti',
+  components: {
+    ButtonDokumenBukti
+  },
+
   data () {
     return {
       dummyFile: [
         'http://101.50.0.202:12002/trk/img/lim/Book1.xlsx',
         'http://101.50.0.202:12002/trk/img/lim/Book1.xlsx',
-        'http://101.50.0.202:12002/trk/img/lim/Book1.xlsx',
+        'http://101.50.0.202:12002/trk/img/lim/Book1.doc',
         'https://www.jotform.com/uploads/JDS_Digital/230227877098062/5714318406876661727/yankes.JPG',
         'https://www.jotform.com/uploads/JDS_Digital/230227877098062/5715109954421580499/Screenshot 2023-09-25 145359.png',
         'https://www.jotform.com/uploads/JDS_Digital/230227877098062/5715120784429903272/Screenshot 2023-09-25 145340.png',
-        'https://www.jotform.com/uploads/JDS_Digital/230227877098062/5597927940986221505/7065e0fc-ff5a-499d-86f6-b420d024c959.jpeg',
-        'blob:https://superapp-admin.staging.digitalservice.id/d4566661-0642-4cf4-bc51-5c1b61d8a7c1.pdf',
-        'blob:https://superapp-admin.staging.digitalservice.id/d4566661-0642-4cf4-bc51-5c1b61d8a7c1.pdf'
+        'https://www.jotform.com/uploads/JDS_Digital/230227877098062/5597927940986221505/7065e0fc-ff5a-499d-86f6-b420d024c959.JPEG',
+        'https://www.africau.edu/images/default/sample.pdf',
+        'https://www.africau.edu/images/default/sample.pdf'
       ],
       grupByTypeFile: {
-        images: { data: [], icon: '' },
-        xlsx: { data: [], icon: '' },
-        pdf: { data: [], icon: '' },
-        documents: { data: [], icon: '' }
+        images: { data: [], icon: '/icon/type-images.png' },
+        xlsx: { data: [], icon: '/icon/type-xls.png' },
+        pdf: { data: [], icon: '/icon/type-pdf.png' },
+        documents: { data: [], icon: '/icon/type-doc.png' }
       }
     }
   },
@@ -45,23 +88,22 @@ export default {
   },
   mounted () {
     this.dummyFile.forEach((url) => {
-      console.log(url)
       const extension = this.fileExtension(url)
       switch (extension) {
         case 'jpg':
         case 'jpeg':
         case 'png':
-          this.grupByTypeFile.images.push(url)
+          this.grupByTypeFile.images.data.push(url)
           break
         case 'xlsx':
-          this.grupByTypeFile.xlsx.push(url)
+          this.grupByTypeFile.xlsx.data.push(url)
           break
         case 'pdf':
-          this.grupByTypeFile.pdf.push(url)
+          this.grupByTypeFile.pdf.data.push(url)
           break
         case 'doc':
         case 'docx':
-          this.grupByTypeFile.documents.push(url)
+          this.grupByTypeFile.documents.data.push(url)
           break
         default:
           break
@@ -70,9 +112,23 @@ export default {
   },
   methods: {
     fileExtension (url) {
-      console.log(url)
       const parts = url.split('.')
       return parts[parts.length - 1]
+    },
+    getLastSegment (url) {
+      const segments = url.split('/')
+      return segments[segments.length - 1]
+    },
+    gotPageFile (file) {
+      if (this.fileExtension(file) === 'pdf') {
+        this.$store.commit('setFileAduan', file)
+        this.$router.push('/aduan-warga/file-aduan/files')
+      } else if (['jpg', 'jpeg', 'png'].includes(this.fileExtension(file))) {
+        this.$store.commit('setFileAduan', file)
+        this.$router.push('/aduan-warga/file-aduan/images')
+      } else {
+        window.location.href = file
+      }
     }
   }
 }
