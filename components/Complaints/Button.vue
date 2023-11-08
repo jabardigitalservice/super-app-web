6a92d817d6eed1231a89f250d6488d9c48fa162a
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2023-11-08 14:09:44 +0800

    feat: add component button complaints

---
+++ components/Complaints/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseButton class="rounded-lg border-2 border-gray-400" :href="config.linkApps.android">
+    <BaseIconSvg
+      :icon="`/icon/${icon}.svg`"
+      mode="image"
+      class="!w-[24px] !h-[24px]"
+    />
+    <span
+      class="text-[14px] font-bold font-lato leading-[20px] text-gray-800 dark:text-dark-text-medium"
+    >{{ textButton }}</span>
+  </BaseButton>
+</template>
+
+<script>
+export default {
+  name: 'ComplaintsButton',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    textButton: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style></style>
