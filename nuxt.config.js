0a99405668fa31ca64dcf950a32394bc1b58464f
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2023-11-08 14:09:19 +0800

    chore:add env link android and ios

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,8 @@ export default {
       }
     },
     googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
-    baseURLAduan: { url: process.env.BASE_URL_ADUAN, username: process.env.USERNAME_URL_ADUAN, password: process.env.PASSWORD_URL_ADUAN }
+    baseURLAduan: { url: process.env.BASE_URL_ADUAN, username: process.env.USERNAME_URL_ADUAN, password: process.env.PASSWORD_URL_ADUAN },
+    linkApps: { android: process.env.LINK_SAPAWARGA_ANDROID, ios: process.env.LINK_SAPAWARGA_IOS }
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
