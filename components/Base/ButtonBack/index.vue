1eeffbfa8121f7b0ce24a1a6a9f6a5d78707bd58
Ray Fajar Salinggih <rayfajars@gmail.com>
2024-02-01 17:14:16 +0700

    feat: create back button to first page

---
+++ components/Base/ButtonBack/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="$route.query.origin" class="font-bold h-[50px] border-b bg-white flex items-center dark:border-dark-emphasis-medium dark:bg-black">
+    <button class="flex items-center" @click="$router.go(-1)">
+      <BaseIconSvg :icon="`/icon/arrow-left.svg`" class="w-[44px] h-[44px] bg-black dark:!bg-white ml-3" />
+      <span class="text-gray-900 dark:text-dark-text-high ml-2">Kembali</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseButtonBack'
+
+}
+</script>
