6e5b1998d29f4f7b2ac875ca1921ae1a6eb8df99
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-01-31 15:53:48 +0700

    feat: add component tracking complaint

--- components/TrackingComplaint/Search.vue
+++ components/TrackingComplaint/Search.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="max-w-xl lg:max-w-2xl w-full">
-    <div class="tracking-complaint--search relative mb-8">
-      <JdsSearch
-        placeholer="Masukkan ID aduan"
-        icon
-        button
-        value=""
-        v-on="$listeners"
+  <div class="max-w-xl lg:max-w-2xl w-full flex flex-row gap-x-3">
+    <div
+      class="relative flex items-center w-full border border-gray-300 rounded-[10px]"
+    >
+      <div class="absolute left-4 flex items-center">
+        <BaseIconSvg
+          icon="/icon/search.svg"
+          class="!w-5 !h-5"
+          fillColor="#000000"
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="Masukan ID Aduan"
+        class="w-full  pl-12 pr-4 text-gray-700 bg-transparent focus:outline-none text-sm"
+        v-model="search"
       />
     </div>
+    <BaseButton
+      class="text-base font-bold text-white mr-2 dark:border-0 bg-green-700 hover:bg-green-600"
+      @click="handleSearch"
+    >
+      <BaseIconSvg
+        icon="/icon/search.svg"
+        class="!w-5 !h-5"
+        fillColor="#FFFFFF"
+      />
+    </BaseButton>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    handleSearch() {
+      console.log('test:', this.search)
+    },
+  },
+}
+</script>
