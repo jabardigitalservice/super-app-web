e15e47e9133e1b707681c2eced40779419de14c6
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-01-31 15:54:43 +0700

    chore: add rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'eslint:recommended'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "prettier",
+    "eslint:recommended",
   ],
   plugins: [],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': 0
-  }
-}
+    "vue/multi-word-component-names": 0,
+  },
+};
