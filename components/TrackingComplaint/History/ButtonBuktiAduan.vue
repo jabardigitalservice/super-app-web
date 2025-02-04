7b46f0b3886383b673a37b4532553cb54d52db53
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-04 09:28:56 +0700

    style: button bukti aduan

--- components/TrackingComplaint/History/ButtonBuktiAduan.vue
+++ components/TrackingComplaint/History/ButtonBuktiAduan.vue
@@ -1,47 +1,27 @@
 <template>
-  <BaseButtonCustom
-    class="!flex !justify-start bg-[#F9F9F9] text-[12px] font-lato rounded-lg dark:bg-dark-emphasis-medium w-full !px-3 !py-2 mt-2 border-0"
-    @click="goToPageFile()"
-  >
-    <BaseButtonBodyCustom>
-      <BaseIconSvg
-        icon="/icon/image-and-document.svg"
-        class="!w-[14px] !h-[14px]"
-        :fill-color="'#757575'"
-      />
-      <span :class="textClass">Dokumen Bukti</span>
-      <span class="ml-auto font-bold text-green-600">Lihat</span>
-    </BaseButtonBodyCustom>
-  </BaseButtonCustom>
+  <BaseButton class="border-green-700 hover:bg-green-50 mt-2 !py-2">
+    <BaseIconSvg
+      icon="/icon/image-and-document.svg"
+      class="!w-[14px] !h-[14px]"
+      :fill-color="'#039550'"
+    />
+    <span class="text-green-700 text-sm font-bold"> Dokumen Bukti </span>
+  </BaseButton>
 </template>
 
 <script>
-import BaseButtonCustom from '~/components/Base/ButtonCustom/Button.vue'
-import BaseButtonBodyCustom from '~/components/Base/ButtonCustom/BodyButton.vue'
-
 export default {
-  components: {
-    BaseButtonCustom,
-    BaseButtonBodyCustom
-  },
   props: {
     index: {
       type: Number,
       required: true,
-      default: 0
-    }
-  },
-  computed: {
-    textClass () {
-      return this.index > 0
-        ? 'font-medium text-gray-500 dark:text-dark-text-low'
-        : 'font-medium text-gray-900 dark:text-dark-text-high'
-    }
+      default: 0,
+    },
   },
   methods: {
-    goToPageFile () {
+    goToPageFile() {
       this.$emit('show-file')
-    }
-  }
+    },
+  },
 }
 </script>
