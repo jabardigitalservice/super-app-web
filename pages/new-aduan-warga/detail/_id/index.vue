8febd25fa2a5591f6043d9fa1ab97459a514b71d
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-06 14:23:45 +0700

    feat: integration header and badge

--- pages/new-aduan-warga/detail/_id/index.vue
+++ pages/new-aduan-warga/detail/_id/index.vue
@@ -7,7 +7,20 @@
       class="border px-[26px] py-7 border-gray-300 rounded-lg mx-auto flex flex-col gap-[16px]"
     >
       <div class="flex flex-row justify-between">
-        <TrackingComplaintHeader />
+        <div class="flex flex-row gap-1 items-center">
+          <BaseIconSvg
+            icon="/icon/tracking-aduan.svg"
+            :width="51"
+            :height="51"
+            mode="image"
+          />
+          <div>
+            <h2 class="font-lato text-sm font-medium text-black">
+              Tracking Aduan
+            </h2>
+            <p class="text-sm font-lato text-green-700">SPW-000055667</p>
+          </div>
+        </div>
         <TrackingComplaintBadge class="self-center" status="NONE" />
       </div>
 
@@ -22,18 +35,9 @@
       />
 
       <div>
-        <!-- <keep-alive>
-          <component
-            :is="currentTabComponent"
-            :data-aduan="dataAduan"
-            :loading="loading"
-          />
-        </keep-alive> -->
-
         <keep-alive>
           <component
             :is="currentTabComponent"
-            v-if="currentTabComponent"
             :data-aduan="dataAduan"
             :loading="loading"
           />
