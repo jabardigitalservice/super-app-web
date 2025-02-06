d96b9de23d1960c88c1f16a2e399e853f01fdb4f
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-06 14:35:52 +0700

    test status

--- pages/new-aduan-warga/detail/_id/index.vue
+++ pages/new-aduan-warga/detail/_id/index.vue
@@ -6,23 +6,23 @@
     <div
       class="border px-[26px] py-7 border-gray-300 rounded-lg mx-auto flex flex-col gap-[16px]"
     >
-      <div class="flex flex-row justify-between">
-        <div class="flex flex-row gap-1 items-center">
+      <!-- <div class="flex flex-row justify-between"> -->
+        <!-- <div class="flex flex-row gap-1 items-center"> -->
           <!-- <BaseIconSvg
             icon="/icon/tracking-aduan.svg"
             :width="51"
             :height="51"
             mode="image"
           /> -->
-          <div>
+          <!-- <div>
             <h2 class="font-lato text-sm font-medium text-black">
               Tracking Aduan
             </h2>
             <p class="text-sm font-lato text-green-700">SPW-000055667</p>
           </div>
-        </div>
+        </div> -->
         <!-- <TrackingComplaintBadge class="self-center" status="NONE" /> -->
-      </div>
+      <!-- </div> -->
 
       <hr />
 
