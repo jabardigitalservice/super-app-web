5d181e49cf90c0164bd6335c6108be4d65262918
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-06 13:33:40 +0700

    test search aduan

--- pages/new-aduan-warga/detail/_id/index.vue
+++ pages/new-aduan-warga/detail/_id/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full p-4 bg-white min-h-screen">
     <div class="mb-5">
-      <!-- <TrackingComplaintSearch /> -->
+      <TrackingComplaintSearch />
     </div>
     <div
       class="border px-[26px] py-7 border-gray-300 rounded-lg mx-auto flex flex-col gap-[16px]"
