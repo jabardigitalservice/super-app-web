5e8ac56669bcafdef57118ac4b7dc15f7c7867a9
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-06 12:29:53 +0700

    feat: test aduan

--- pages/new-aduan-warga/detail/_id/index.vue
+++ pages/new-aduan-warga/detail/_id/index.vue
@@ -1,48 +1,15 @@
 <template>
-  <div class="w-full h-full p-4 bg-white min-h-screen">
-    <div class="mb-5">
-      <TrackingComplaintSearch />
-    </div>
-    <div
-      class="border px-[26px] py-7 border-gray-300 rounded-lg mx-auto flex flex-col gap-[16px]"
-    >
-      <div class="flex flex-row justify-between">
-        <TrackingComplaintHeader />
-        <TrackingComplaintBadge class="self-center" status="NONE" />
-      </div>
-
-      <hr />
-
-      <TrackingComplaintFooter />
-
-      <TrackingComplaintSwitcher
-        class="justify-self-start"
-        :items="tabItems"
-        @click="getSelected($event)"
-      />
-
-      <div>
-
-
-        <!-- <keep-alive>
-          <component
-            :is="currentTabComponent"
-            v-if="currentTabComponent"
-            :data-aduan="dataAduan"
-            :loading="loading"
-          />
-        </keep-alive> -->
-      </div>
-    </div>
-  </div>
+ <div>
+  test aduan
+ </div>
 </template>
 
 <script>
 import { fetchAduanData } from '~/utils'
 export default {
   components: {
-    HistoryComplaint: () => import('@/components/TrackingComplaint/History'),
-    DetailComplaint: () => import('@/components/TrackingComplaint/Detail'),
+    // HistoryComplaint: () => import('@/components/TrackingComplaint/History'),
+    // DetailComplaint: () => import('@/components/TrackingComplaint/Detail'),
   },
   async asyncData({ $aduanAPI, params, $config }) {
     let loading = true
