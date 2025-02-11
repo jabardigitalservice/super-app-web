69096f0b7a95e967aec7716d9d4572ccf3929f51
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-11 15:02:55 +0700

    feat: add component icons

---
+++ components/Icon/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <img
+    v-if="src"
+    :src="src"
+    :alt="alt"
+    :width="size"
+    :height="size"
+    @click="onClick"
+  >
+  <div v-else class="flex justify-center items-center" :style="{ width: size, height: size }" @click="onClick">
+    <JdsIcon :name="name" :size="size" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: null
+    },
+    size: {
+      type: String,
+      required: false,
+      default: null
+    },
+    src: {
+      type: String,
+      required: false,
+      default: null
+    },
+    alt: {
+      type: String,
+      required: false,
+      default: null
+    }
+  },
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
