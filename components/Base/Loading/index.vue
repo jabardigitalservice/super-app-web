11afac817236ca51cab5df549c7847496ee834b8
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-12 10:33:39 +0700

    feat: add base loading

---
+++ components/Base/Loading/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="border-t-transparent border-solid animate-spin rounded-full border-green-600 border-8"
+    :style="`width: ${width}; height: ${height}`"
+  />
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner',
+  props: {
+    height: {
+      type: String,
+      default: '56px',
+    },
+    width: {
+      type: String,
+      default: '56px',
+    },
+  },
+}
+</script>
