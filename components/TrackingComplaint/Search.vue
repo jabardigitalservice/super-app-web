508449c93843192ad378adef82ddc7ed7eec4a81
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-02-18 11:31:25 +0700

    feat: add vee validate search aduan

--- components/TrackingComplaint/Search.vue
+++ components/TrackingComplaint/Search.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="max-w-xl lg:max-w-2xl w-full flex flex-row gap-x-3">
-    <div
-      class="relative flex items-center w-full border border-gray-300 rounded-[10px]"
+  <div class="max-w-xl lg:max-w-2xl w-full flex items-center">
+    <ValidationObserver
+      v-slot="{ invalid }"
+      class="flex items-center w-full max-w-xl"
     >
-      <div class="absolute left-4 flex items-center">
+      <ValidationProvider
+        v-slot="{ errors }"
+        tag="div"
+        class="flex-1"
+        name="ID Aduan"
+        rules="required|min:5"
+      >
+        <JdsInputText
+          id="search"
+          v-model="searchQuery"
+          class="w-full text-gray-700 bg-transparent focus:outline-none text-sm"
+          :error-message="errors[0]"
+          placeholder="Masukan ID Aduan"
+        >
+          <template #prefix-icon>
+            <jds-icon name="magnifier" size="1em" fill="#bdbdbd" />
+          </template>
+        </JdsInputText>
+      </ValidationProvider>
+      <BaseButton
+        class="text-base font-bold text-white ml-2 dark:border-0 bg-green-700 hover:bg-green-600"
+        :disabled="invalid"
+        @click="handleInput"
+      >
         <BaseIconSvg
           icon="/icon/search.svg"
           class="!w-5 !h-5"
-          fillColor="#000000"
+          fill-color="#FFFFFF"
         />
-      </div>
-      <input
-        v-model="searchQuery"
-        type="text"
-        placeholder="Masukan ID Aduan"
-        class="w-full pl-12 pr-4 text-gray-700 bg-transparent focus:outline-none text-sm"
-      />
-    </div>
-    <BaseButton
-      class="text-base font-bold text-white mr-2 dark:border-0 bg-green-700 hover:bg-green-600"
-      @click="handleInput"
-    >
-      <BaseIconSvg
-        icon="/icon/search.svg"
-        class="!w-5 !h-5"
-        fillColor="#FFFFFF"
-      />
-    </BaseButton>
+      </BaseButton>
+    </ValidationObserver>
   </div>
 </template>
 
