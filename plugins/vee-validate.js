8239ec8dcab3b88fff00185e18c9f15dc2a599f9
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-03-11 10:19:03 +0700

    feat: add vee-validate phone number

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,68 +1,73 @@
 /* eslint-disable camelcase */
-import Vue from 'vue'
 import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
 import {
-  required,
+  email,
+  image,
+  length,
+  max,
+  mimes,
+  min,
   min_value,
   numeric,
-  email,
-  min,
-  max,
-  length,
-  image,
+  required,
   size,
-  mimes
 } from 'vee-validate/dist/rules'
+import Vue from 'vue'
 
 Vue.component('ValidationObserver', ValidationObserver)
 Vue.component('ValidationProvider', ValidationProvider)
 
 extend('required', {
   ...required,
-  message: field => field + ' belum diisi'
+  message: (field) => field + ' belum diisi',
 })
 
 extend('numeric', {
   ...numeric,
-  message: field => `${field} hanya menerima karakter angka`
+  message: (field) => `${field} hanya menerima karakter angka`,
 })
 
 extend('min_value', {
   ...min_value,
-  message: 'Nomor urut dimulai dari angka 1'
+  message: 'Nomor urut dimulai dari angka 1',
 })
 
 extend('email', {
   ...email,
-  message: field => `${field} yang anda masukkan salah!`
+  message: (field) => `${field} yang anda masukkan salah!`,
 })
 
 extend('min', {
   ...min,
-  message: (field, values) => `${field} minimal ${values.length} karakter`
+  message: (field, values) => `${field} minimal ${values.length} karakter`,
 })
 
 extend('length', {
   ...length,
-  message: field => `${field} harus 16 digit`
+  message: (field) => `${field} harus 16 digit`,
 })
 
 extend('max', {
   ...max,
-  message: (field, values) => `${field} maksimal ${values.length} karakter`
+  message: (field, values) => `${field} maksimal ${values.length} karakter`,
 })
 
 extend('image', {
   ...image,
-  message: 'File yang anda pilih gambar!'
+  message: 'File yang anda pilih gambar!',
 })
 
 extend('size', {
   ...size,
-  message: 'File yang anda masukkan melebihi 1 Mb'
+  message: 'File yang anda masukkan melebihi 1 Mb',
 })
 
 extend('mimes', {
   ...mimes,
-  message: 'File yang anda masukkan harus berformat .jpg atau .png'
+  message: 'File yang anda masukkan harus berformat .jpg atau .png',
+})
+
+extend('phoneNumber', {
+  validate: (value) => value.startsWith('08'),
+  message: (_, values) => `${values._field_} harus dimulai dengan 08`,
 })
