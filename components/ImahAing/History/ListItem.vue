946c00b1d3f18fa69dc586bbcd4694258784f977
ekadhirapratama <eka.dhirapratama@gmail.com>
2026-06-23 15:17:13 +0700

    fix(imah-aing): make info icon clickable with dismissible snackbar tooltip

--- components/ImahAing/History/ListItem.vue
+++ components/ImahAing/History/ListItem.vue
@@ -58,7 +58,7 @@
           type="button"
           class="w-5 h-5 flex items-center justify-center rounded-full text-gray-400 hover:text-gray-600 hover:bg-gray-100 transition-colors flex-shrink-0"
           :title="editTooltipText"
-          @click.stop
+          @click.stop="$emit('show-info')"
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <circle cx="12" cy="12" r="10"/>
